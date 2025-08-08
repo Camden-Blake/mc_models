a95d9e5367cae9b9672e7395b92f1855765b5d14
Camden Blake <wvcamdenblake@gmail.com>
2025-08-08 11:31:41 -0400

    Rename mcnpy resulting file

--- CROCUS/OpenMC/convert_openmc_to_mcnp.py
+++ CROCUS/OpenMC/convert_openmc_to_mcnp.py
@@ -7,4 +7,4 @@ settings_file = "settings.xml"
 
 mcnp_deck = mcnpy.translate_mcnp_openmc.translate_file(geometry_file, materials_file, settings_file)
 
-mcnp_deck.write("crocus.mcnp")
+mcnp_deck.write("mcnpy_crocus.mcnp")
